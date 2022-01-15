4a9305b9e2936a8697fbfbca22eff5d1c126367c
Tiago Bernardo <hsbtiago@live.com>
2022-01-15 07:29:15 -0400

    Criado console app

---
+++ Console/Program.cs
@@ -0,0 +1,26 @@
+﻿using Dominio;
+
+Console.WriteLine("Olá! Por favor digite um número:");
+
+var valorDigitado = Console.ReadLine();
+
+var numeroDigitadoCorretamente = int.TryParse(valorDigitado, out int numeroDigitado);
+
+if (!numeroDigitadoCorretamente)
+{
+    Console.WriteLine("Aceitamos somente números inteiros! Para adicionar essa funcionalidade favor tratar com o comercial xD");
+    return;
+}
+
+var divisores = new CalcularDivisores().Executar(numeroDigitado);
+
+Console.WriteLine("Os divisores desses número são:");
+divisores.ForEach(f => Console.WriteLine(f));
+
+Console.WriteLine("Os números primos desse número são:");
+
+var primos = divisores
+    .Where(s => s.ÉNúmeroPrimo())
+    .ToList();
+
+primos.ForEach(f => Console.WriteLine(f));
